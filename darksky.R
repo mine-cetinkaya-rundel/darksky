6a83adf6b4b707bc647bf899a4442f958b4e2356
Mine Cetinkaya-Rundel <mine@stat.duke.edu>
2019-05-15 13:38:55 +0100

    RDU ✈️ EDI

--- darksky.R
+++ darksky.R
@@ -4,27 +4,27 @@ library(tidyverse)
 library(lubridate)
 
 # set location and time and locale ---------------------------------------------
-RDU <- list(lat = 35.8801, lon = -78.7880, time = "2019-05-15T00:00:00-04:00")
+EDI <- list(lat = 55.9508, lon = -3.3615,  time = "2019-05-15T00:00:00+01:00")
 
 # get forecast -----------------------------------------------------------------
-rdu_forecast <- get_forecast_for(RDU$lat, RDU$lon, RDU$time, exclude = "daily,currently")
+edi_forecast <- get_forecast_for(EDI$lat, EDI$lon, EDI$time, exclude = "daily,currently")
 
 # plot avg apparent temperature throughout day ---------------------------------
-us_blue <- "#002664"
+scot_blue <- "#0165BF"
 
-rdu_forecast$hourly %>% 
+edi_forecast$hourly %>% 
   mutate(
-    hour = hour(time) - 5, # because package converts to local time
+    hour = hour(time),
     time_of_day = cut(hour, breaks = 4, labels = c("night", "morning", "afternoon", "evening"))
   ) %>%
   group_by(time_of_day) %>%
   summarize(mean_temp = mean(apparentTemperature)) %>%
   ggplot(aes(x = time_of_day, y = mean_temp)) +
-    geom_line(group = 1, color = us_blue)  + 
-    geom_point(color = us_blue) +
+    geom_line(group = 1, color = scot_blue) + 
+    geom_point(color = scot_blue) +
     labs(x = "", y = "Mean temperature (F)", 
          title = "Apparent temperature",
-         subtitle = "May 15, 2019 - Durham, NC") +
+         subtitle = "May 15, 2019 - Edinburgh, UK") +
     scale_y_continuous(limits = c(40, 80),
                        sec.axis = sec_axis(trans = ~(. - 32) * (5/9), 
                                            name = "Mean temperature (C)"))
